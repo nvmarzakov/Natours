b47ceaa3fed1aea5c6ae18d1a341fdaf18a5efa1
nvmarzakov <nikolay.marzakov@gmail.com>
2024-11-03 16:37:54 +0200

    section header - heading text

--- src/resources/views/layouts/partials/header.blade.php
+++ src/resources/views/layouts/partials/header.blade.php
@@ -2,4 +2,11 @@
     <div class="logo-box">
         <img src="{{ asset('images/logo-white.png') }}" alt="Logo" class="logo">
     </div>
+
+    <div class="text-box">
+        <h1 class="heading-primary">
+            <span class="heading-primary-main">Outdoors</span>
+            <span class="heading-primary-sub">is where life happens</span>
+        </h1>
+    </div>
 </header>
