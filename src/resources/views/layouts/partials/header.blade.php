b8b23bcd44b2fa02217c77d2399ac245a2784e3b
nvmarzakov <nikolay.marzakov@gmail.com>
2024-11-15 09:15:49 +0200

    implemeting BEM

--- src/resources/views/layouts/partials/header.blade.php
+++ src/resources/views/layouts/partials/header.blade.php
@@ -1,14 +1,14 @@
 <header class="header">
-    <div class="logo-box">
-        <img src="{{ asset('images/logo-white.png') }}" alt="Logo" class="logo">
+    <div class="header__logo-box">
+        <img src="{{ asset('images/logo-white.png') }}" alt="Logo" class="header__logo">
     </div>
 
-    <div class="text-box">
+    <div class="header__text-box">
         <h1 class="heading-primary">
-            <span class="heading-primary-main">Outdoors</span>
-            <span class="heading-primary-sub">is where life happens</span>
+            <span class="heading-primary--main">Outdoors</span>
+            <span class="heading-primary--sub">is where life happens</span>
         </h1>
         
-        <a href="#" class="button button-white button-animated">Discover our tours</a>
+        <a href="#" class="button button--white button--animated">Discover our tours</a>
     </div>
 </header>
